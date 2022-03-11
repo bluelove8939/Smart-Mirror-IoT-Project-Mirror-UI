1e2ded5b05f45a488cfd9df75e3ab20877837174
Seongwook Kim <bluelove8939@naver.com>
2022-03-11 19:17:38 +0900

    Update fonts and layouts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from PyQt5.QtWidgets import QApplication, QWidget
@@ -7,30 +8,32 @@ from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout  # Layouts
 from PyQt5.QtCore import QDate          # Date widgets
 from PyQt5.QtCore import QTime, QTimer  # Time widgets
 from PyQt5.QtCore import Qt             # Qt.AlignCenter
-from PyQt5.QtCore import QCoreApplication
 
-from PyQt5.QtGui import QIcon, QFont, QImage, QPixmap
+from PyQt5.QtGui import QIcon, QFont, QImage, QPixmap, QFontDatabase
 
 from dataManager import WeatherDownloader
 
 
+# Main user interface
+#   Indicates information about current datetime, scheudles, weather ...
+
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+
         # Global variables
         self.weatherDownloader = WeatherDownloader()
 
         # Window Settings
         self.setWindowTitle('Smart Mirror System')
         self.setWindowIcon(QIcon('assets/title_icon.png'))
-        self.setStyleSheet('background-color:black')
+        self.setStyleSheet('background-color:black;')
         self.setGeometry(0, 0, 700, 1000)
 
         # Time widget
         self.dateTimeWidget = QLabel()
-        self.dateTimeWidget.setStyleSheet('color: white')
         self.dateTimeWidget.setAlignment(Qt.AlignCenter)
-        self.dateTimeWidget.setFont(QFont('Arial', 30, QFont.Bold))
+        self.dateTimeWidget.setStyleSheet("color: white; font-size: 36px")
 
         # Schedule widget
         self.scheduleWidget = self.generateScheduleWidget()
@@ -38,13 +41,14 @@ class MyApp(QWidget):
         # Weather widget
         self.weatherWidget = self.generateWeatherWidget()
 
-        # Run timer
+        # Run timer (time widget)
         timer = QTimer(self)  # run timer widget
         timer.timeout.connect(self.showTime)  # refresh label widget when timeout called
         timer.start(1000)
 
         # Generating layout
         mainLayout = QVBoxLayout()
+        mainLayout.setContentsMargins(20, 20, 20, 20)
         mainLayout.addWidget(self.dateTimeWidget)
         mainLayout.addStretch(1)
 
@@ -61,7 +65,7 @@ class MyApp(QWidget):
 
     def showTime(self):
         currentTime = QTime.currentTime().toString('hh:mm:ss')
-        currentDate = QDate.currentDate().toString('yyyy-MM-dd ddd')
+        currentDate = QDate.currentDate().toString('yyyy-MM-dd dddd')
         self.dateTimeWidget.setText(f'{currentDate} {currentTime}')  # change timeWidget
 
     def generateScheduleWidget(self):
@@ -77,6 +81,7 @@ class MyApp(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addWidget(label)
+        vbox.setContentsMargins(20, 20, 20, 20)
         groupbox.setLayout(vbox)
 
         return groupbox
@@ -93,7 +98,7 @@ class MyApp(QWidget):
         # Download weather data
         weatherDataJson = self.weatherDownloader.download()
 
-        # Generate weather sub widgets
+        # Generate weather subwidgets
         cityNameWidget = QLabel(f"{str(weatherDataJson.get('name'))}")
         temperatureWidget = QLabel(f"현재기온 {str(weatherDataJson.get('main').get('temp'))}°C")
         minMaxTemperatureWidget = QLabel(f"(최소 {str(weatherDataJson.get('main').get('temp_min'))}°C 최대 {str(weatherDataJson.get('main').get('temp_max'))}°C)")
@@ -102,9 +107,9 @@ class MyApp(QWidget):
         # Generate weather icon widget
         weatherIconImage = QImage()
         weatherIconImage.loadFromData(self.weatherDownloader.downloadWeatherIcon(str(weatherDataJson.get('weather')[0].get('icon'))))
-        weatherIconwidget = QLabel()
         weatherIconPixmap = QPixmap(weatherIconImage)
         weatherIconPixmap.scaledToHeight(64)
+        weatherIconwidget = QLabel()
         weatherIconwidget.setPixmap(weatherIconPixmap)
 
 
@@ -114,18 +119,31 @@ class MyApp(QWidget):
         humidityWidget.setStyleSheet('font-size: 13pt; font-weight: bold')
         minMaxTemperatureWidget.setStyleSheet('font-size: 10pt; font-weight: bold')
 
+        # Generate layouts fo subwidgets and return weather widget
         vbox = QVBoxLayout()
         vbox.addWidget(weatherIconwidget)
         vbox.addWidget(cityNameWidget)
         vbox.addWidget(temperatureWidget)
         vbox.addWidget(minMaxTemperatureWidget)
         vbox.addWidget(humidityWidget)
+        vbox.setContentsMargins(20, 0, 20, 20)
         groupbox.setLayout(vbox)
 
         return groupbox
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = MyApp()
+
+    # Set font as SUIT
+    #   This font is open-source font from the link below
+    #   URL: https://sunn.us/suit/
+    fontDirname = os.path.join(os.getcwd(), 'assets', 'fonts')
+    fontFilenames = [filename for filename in os.listdir(fontDirname) if
+                     os.path.isfile(os.path.join(fontDirname, filename))]
+    fontIds = []
+    for targetFilename in fontFilenames: fontIds.append(QFontDatabase.addApplicationFont(os.path.join(fontDirname, targetFilename)))
+    app.setFont(QFont('SUIT'))
+
+    sys.exit(app.exec_())
