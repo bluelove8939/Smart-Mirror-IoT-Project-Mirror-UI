91b29b8c8cade445b4e1f1daa7d3c362219cfe23
Seongwook Kim <bluelove8939@naver.com>
2022-03-11 14:20:51 +0900

    Initial commit (basic interface structures with weather widget)

--- dataManager.py
+++ dataManager.py
@@ -3,11 +3,26 @@ import requests
 import json
 
 
+# Reading required apikeys
+#
+# Note:
+#   API keys is at file named 'assets/keys/apikeys.txt'
+#   Make your own CSV file for API keys
+#   Or manually generate the 'apikeys' dictionary
+
+apikeys = {}
+
+with open(os.path.join('assets', 'keys', 'apikeys.txt'), 'rt') as keyfile:
+    content = list(map(lambda x: x.split(','), keyfile.readlines()))
+    for keyname, keycontent in content:
+        apikeys[keyname] = keycontent
+
+
 class WeatherDownloader:
     def __init__(self):
         self.lat = 37.29410404832249
         self.lon = 126.97533756753458
-        self.apikey = 'bfbfaac336f64b9f2b20f8b1bb583e56'
+        self.apikey = apikeys['openweathermap']
 
     def download(self):
         URL = f'http://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.apikey}&lang=kr&units=metric'
