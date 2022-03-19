877b2d3a10e93cbd7306af761304a854877fc130
Seongwook Kim <bluelove8939@naver.com>
2022-03-20 02:16:51 +0900

    Update installing libraries for google login

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -1,3 +1,9 @@
+# Install pip for linux system
+sudo apt-get install python-pip
+
 # Installing pyqt5 python library
 sudo apt-get install python3-pyqt5
 sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools
+
+# Installing google
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
