e9769cf17ec0409af9b44c6c76d485800c2cce5c
Seongwook Kim <bluelove8939@naver.com>
2022-03-20 02:16:30 +0900

    Add bash file for windows setup

---
+++ windows-setup.sh
@@ -0,0 +1,5 @@
+# Installing pyqt5 python library
+pip install pyqt5
+
+# Installing google authorization libraries
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
