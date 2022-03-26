bfc53c226033e318dba612b4224f003829c4e14e
bluelove8939 <bluelove8939@naver.com>
2022-03-26 22:40:16 +0900

    Update sdptool bluetooth initial setup

--- raspberrypi-bluetooth-setup.sh
+++ raspberrypi-bluetooth-setup.sh
@@ -1,3 +1,6 @@
 bluetoothctl -- system-alias 'Smart Mirror(SSS001)'
 bluetoothctl -- discoverable-timeout 0  # device is always discoverable
 bluetoothctl -- discoverable on  # turn on discoverable
+
+sudo sdptool add --channel=1 SP  # add RFCOMM service to port 1
+sudo chmod 777 /var/run/sdp  # sdp permission setting
