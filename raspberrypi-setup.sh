02fc18cfe76ffab265db5b7701c942ed1683b3e8
bluelove8939 <bluelove8939@naver.com>
2022-04-01 17:16:24 +0900

    Update raspberry pi setup bash

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -9,5 +9,6 @@ sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools
 sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 # Installing bluetooth module and bluetooth python lib
+sudo apt-get install python3-dev libbluetooth-dev
 sudo apt-get install bluetooth blueman bluez
 sudo apt-get install python-pybluez  # python bluetooth extension
