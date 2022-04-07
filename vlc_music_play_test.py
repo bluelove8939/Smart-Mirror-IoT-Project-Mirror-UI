a7fa9e6671ccf81c746132e9765d2802aca42af7
Seongwook Kim <bluelove8939@naver.com>
2022-04-08 00:43:47 +0900

    Add youtube music streaming test code by using pafy and vlc player

---
+++ vlc_music_play_test.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pafy
+import vlc
+
+
+os.add_dll_directory(os.getcwd())
+
+apikeys = {}
+apikeysDirectoryPath = os.path.join(os.path.curdir, 'assets', 'keys')
+
+with open(os.path.join('assets', 'keys', 'apikeys.json'), 'rt') as keyFile:
+    readApiKeys = json.loads(keyFile.read())
+    for k, v in readApiKeys.items():
+        apikeys[k] = v
+
+status = 0
+
+def test_callback_func(event):
+    print('callback called')
+    global status
+    status = 1
+
+if "youtubeapikey" in apikeys.keys():
+    pafy.set_api_key(apikeys["youtubeapikey"])
+
+url = "https://www.youtube.com/watch?v=L6HRp9GUTT0"
+video = pafy.new(url)
+best = video.getbest()
+playurl = best.url
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
+Media.get_mrl()
+player.set_media(Media)
+player.play()
+
+while status == 0:
+    continue
