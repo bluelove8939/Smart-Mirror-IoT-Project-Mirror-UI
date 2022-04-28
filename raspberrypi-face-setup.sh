500f9e06c44d965e6e12f6bc139859957d31ac42
bluelove8939 <bluelove8939@naver.com>
2022-04-28 13:54:56 +0900

    Update face emotion detection feature (testing)

---
+++ raspberrypi-face-setup.sh
@@ -0,0 +1,3 @@
+# Installing required packages for face detection and azure face API
+python3 -m pip install datetime imutils opencv-python requests Pillow
+python3 -m pip install --upgrade azure-cognitiveservices-vision-face
